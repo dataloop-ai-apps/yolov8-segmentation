f70948aaf9388246f8573c073e61b79ea51af6f3
OfirDataloopAI <ofir.g@dataloop.ai>
2024-11-10 17:07:23 +0200

    fixed predict to support only images

--- model_adapter.py
+++ model_adapter.py
@@ -314,25 +314,35 @@ class Adapter(dl.BaseModelAdapter):
 
     def prepare_item_func(self, item):
         filename = item.download(overwrite=True)
-        image = Image.open(filename)
-        # Check if the image has EXIF data
-        if hasattr(image, '_getexif'):
-            exif_data = image._getexif()
-            # Get the EXIF orientation tag (if available)
-            if exif_data is not None:
-                orientation = exif_data.get(0x0112)
-                if orientation is not None:
-                    # Rotate the image based on the orientation tag
-                    if orientation == 3:
-                        image = image.rotate(180, expand=True)
-                    elif orientation == 6:
-                        image = image.rotate(270, expand=True)
-                    elif orientation == 8:
-                        image = image.rotate(90, expand=True)
-        image = image.convert('RGB')
-        return image
+        if 'image' in item.mimetype:
+            data = Image.open(filename)
+            # Check if the image has EXIF data
+            if hasattr(data, '_getexif'):
+                exif_data = data._getexif()
+                # Get the EXIF orientation tag (if available)
+                if exif_data is not None:
+                    orientation = exif_data.get(0x0112)
+                    if orientation is not None:
+                        # Rotate the image based on the orientation tag
+                        if orientation == 3:
+                            data = data.rotate(180, expand=True)
+                        elif orientation == 6:
+                            data = data.rotate(270, expand=True)
+                        elif orientation == 8:
+                            data = data.rotate(90, expand=True)
+            data = data.convert('RGB')
+        else:
+            data = filename
+        return data, item
 
     def predict(self, batch, **kwargs):
+        filtered_streams = list()
+        for stream, item in batch:
+            if 'image' in item.mimetype:
+                filtered_streams.append(stream)
+            else:
+                logger.warning(f'Item {item.id} mimetype is not supported. Skipping item prediction')
+
         inference_args = self.configuration.get("inference_args", {})
         inference_conf = inference_args.get("conf", 0.05)
         inference_iou = inference_args.get("iou", 0.7)
@@ -346,7 +356,7 @@ class Adapter(dl.BaseModelAdapter):
         inference_retina_masks = inference_args.get("retina_masks", False)
         if inference_device is None:
             inference_device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-        results = self.model.predict(source=batch,
+        results = self.model.predict(source=filtered_streams,
                                      save=False,
                                      save_txt=False,  # save predictions as labels
                                      conf=inference_conf,
